51c21eb96d421a77196582ff135f9dbb6c057163
csb6 <cblakley15@gmail.com>
2019-11-15 22:38:30 -0700

    Added TermboxApp class to manage functionality related to termbox

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 #include "termbox.h"
 
-void editor_present(int insert_x, int insert_y)
+static inline void present(int insert_x, int insert_y);
+
+class TermboxApp {
+public:
+    int screen_width, screen_height;
+    int insert_x = 0;
+    int insert_y = 0;
+    TermboxApp()
+    {
+	auto status = tb_init();
+	if(status < 0) {
+	    std::cerr << "Error: Termbox failed with code " << status << '\n';
+	    tb_shutdown();
+	    exit(1);
+	}
+	screen_width = tb_width();
+	screen_height = tb_height();
+    }
+    void resize()
+    {
+	screen_width = tb_width();
+	screen_height = tb_height();
+	present(screen_width, screen_height);
+    }
+    ~TermboxApp() { tb_shutdown(); }
+};
+
+class Cell {
+    
+};
+
+static inline void present(int insert_x, int insert_y)
 {
     tb_set_cursor(insert_x, insert_y);
     tb_present();
@@ -9,47 +40,37 @@ void editor_present(int insert_x, int insert_y)
 
 int main()
 {
-    auto status = tb_init();
-    if(status < 0) {
-	std::cerr << "Error: Termbox failed with code " << status << '\n';
-	tb_shutdown();
-	return 1;
-    }
-    int screen_width = tb_width();
-    int screen_height = tb_height();
+    TermboxApp tb;
     tb_event event{};
     bool running = true;
-    int insert_x = 0;
-    int insert_y = 0;
-    tb_set_cursor(insert_x, insert_y);
-    tb_present();
-    while(running && tb_peek_event(&event, 2) != -1) {
+    present(tb.insert_x, tb.insert_y);
+    
+    while(running && tb_peek_event(&event, 1) != -1) {
 	if(event.type != TB_EVENT_KEY) {
 	    continue;
-	}
-	if(event.key == TB_KEY_CTRL_C) {
+	} else if(event.type == TB_EVENT_RESIZE) {
+	    tb.resize();
+	    continue;
+	} else if(event.key == TB_KEY_CTRL_C) {
 	    running = false;
 	} else if(event.ch >= '!' || event.key == TB_KEY_SPACE) {
-	    tb_change_cell(insert_x++, insert_y, event.ch, TB_WHITE, TB_BLACK);
-	    if(insert_x >= screen_width) {
-		++insert_y;
-		insert_x = 0;
+	    tb_change_cell(tb.insert_x++, tb.insert_y, event.ch, TB_WHITE, TB_BLACK);
+	    if(tb.insert_x >= tb.screen_width) {
+		++tb.insert_y;
+		tb.insert_x = 0;
 	    }
-	    tb_set_cursor(insert_x, insert_y);
-	    tb_present();
+	    present(tb.insert_x, tb.insert_y);
 	} else if(event.key == TB_KEY_BACKSPACE2) {
-	    if(insert_y <= 0 && insert_x <= 0) continue;
-	    if(insert_x == 0) {
-		insert_x = screen_width - 1;
-		tb_change_cell(insert_x, --insert_y, ' ', TB_WHITE, TB_BLACK);
+	    if(tb.insert_y <= 0 && tb.insert_x <= 0) continue;
+	    if(tb.insert_x == 0) {
+		tb.insert_x = tb.screen_width - 1;
+		tb_change_cell(tb.insert_x, --tb.insert_y, ' ', TB_WHITE, TB_BLACK);
 	    } else {
-		tb_change_cell(--insert_x, insert_y, ' ', TB_WHITE, TB_BLACK);
+		tb_change_cell(--tb.insert_x, tb.insert_y, ' ', TB_WHITE, TB_BLACK);
 	    }
-	    tb_set_cursor(insert_x, insert_y);
-	    tb_present();
+	    present(tb.insert_x, tb.insert_y);
 	}
     }
 
-    tb_shutdown();
     return 0;
 }
